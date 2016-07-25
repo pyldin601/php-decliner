6bb3e1693b5260a155986d378ab91859d53a5e87
Roman Lakhtadyr <roman@homefs.biz>
2016-07-25 17:28:06 +0300

    Update decliner.php

--- src/decliner.php
+++ src/decliner.php
@@ -5,7 +5,7 @@ namespace RG\Libs\Decliner;
 function decline($number, array $titles)
 {
     $cases = array (2, 0, 1, 1, 1, 2);
-    return $number." ".$titles[ ($number%100 > 4 && $number %100 < 20) ? 2 : $cases[min($number%10, 5)] ];
+    return $number . " " . $titles[($number % 100 > 4 && $number % 100 < 20) ? 2 : $cases[min($number % 10, 5)]];
 }
 
 function make(array $titles)
